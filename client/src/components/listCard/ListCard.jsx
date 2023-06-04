47a526834e58d1230bd15248be73ef020a14b6d0
lgramajo99 <lucianongramajo@gmail.com>
2023-06-03 23:14:31 -0300

    estilos listo

--- client/src/components/listCard/ListCard.jsx
+++ client/src/components/listCard/ListCard.jsx
@@ -1,19 +1,16 @@
 import FirstCard from '../cards/FirstCard';
 import styles from './ListCard.module.css';
 
-function ListCard() {
 
+
+function ListCard() {
 
     return (
         <section className={styles.listcard} >
-        <FirstCard />
-        <FirstCard />
-        <FirstCard />
-        <FirstCard />
-        <FirstCard />
-        <FirstCard />
-        <FirstCard />
-        <FirstCard />
+            <FirstCard />
+            <FirstCard />
+            <FirstCard />
+            <FirstCard />
         </section >
     )
 }
