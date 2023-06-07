a59d749365b9c0c333bba44dcaeeb56d7353658b
lgramajo99 <lucianongramajo@gmail.com>
2023-06-07 15:31:16 -0300

    terminando los detalles

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,18 @@ import './App.css';
 import Nav from './components/nav/Nav.jsx';
 import Footer from './components/footer/Footer.jsx';
 import Inicio from './components/inicio/Inicio';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Detalles from './components/detalles/Detalles';
 import CreateDog from './components/createDog/CreateDog';
 import ErrorPage from './components/errors/ErrorPage';
 import Landing from './components/landing/Landing';
 
 function App() {
+  const location = useLocation()
   return (
     <div className="App">
-      <Nav />
+
+      {(location.pathname === '/') ? null : <Nav />}
       <Routes>
         <Route path='/inicio' element={<Inicio />} />
         <Route path='/detalle/:idRaza' element={<Detalles />} />
@@ -20,7 +22,7 @@ function App() {
         <Route path='/' element={<Landing />} />
 
       </Routes>
-      <Footer />
+      {(location.pathname === '/') ? null : <Footer />}
     </div>
   );
 }
